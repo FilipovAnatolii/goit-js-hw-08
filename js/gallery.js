ec83148aeac0dd9ba57de536b3272bd4054de0c5
Filipov Anatolii <anatolii.filippov@icloud.com>
2025-09-14 16:13:07 +0300

    jshw8

--- js/gallery.js
+++ js/gallery.js
@@ -1,7 +1,7 @@
 const images = [
     {
         preview:
-            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__340.jpg',
+            'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg',
         original:
             'https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg',
         description: 'Hokkaido Flower',
@@ -69,10 +69,10 @@ const gallery = document.querySelector('.gallery');
 const markup = images
     .map(
         ({preview, original, description}) => `
-    <div class="gallery__item">
-      <a class="gallery__link" href="${original}">
+    <div class="gallery-item">
+      <a class="gallery-link" href="${original}">
         <img
-          class="gallery__image"
+          class="gallery-image"
           src="${preview}"
           data-source="${original}"
           alt="${description}"
